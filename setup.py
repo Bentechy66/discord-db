1088c16d450d2c2c045332cfe72f629d505cf095
BenTechy66 <sendbenspam@yahoo.co.uk>
2019-06-05 15:40:05 +0100

    rename this

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='funniest',
+setup(name='discorddb',
       version='0.1',
-      description='The World\'s Worst Discord-Based Database System(tm)'
-      url='http://github.com/bentechy66-discord-db',
+      description='The World\'s Worst Discord-Based Database System(tm)',
+      url='http://github.com/bentechy66/discord-db',
       author='Ben Griffiths',
       author_email='sendbenspam@yahoo.co.uk',
       license='GPLv3',
-      packages=['discord-db'],
+      packages=['discorddb'],
       zip_safe=False)
