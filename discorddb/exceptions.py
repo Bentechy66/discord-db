fe5d13edc0575cccb3e0f9178efeb2f6166519df
BenTechy66 <sendbenspam@yahoo.co.uk>
2019-06-05 15:43:06 +0100

    P E P  8

--- discorddb/exceptions.py
+++ discorddb/exceptions.py
@@ -1,23 +1,29 @@
 # Discord Exceptions
 class Discord404Exception(Exception):
-  pass # No further functionality required
-  
+    pass  # No further functionality required
+
+
 class Discord403Exception(Exception):
-  pass # No further functionality required
-  
+    pass  # No further functionality required
+
+
 # Record / Table Exceptions
 class DataTooLongException(Exception):
-  pass # No further functionality required
-  
+    pass  # No further functionality required
+
+
 # Database Exceptions
 class TableNotFoundException(Exception):
-  pass # No further functionality required
-  
+    pass  # No further functionality required
+
+
 class TooManyTablesException(Exception):
-  pass # No further functionality required
-  
+    pass  # No further functionality required
+
+
 class TooLongFieldNamesException(Exception):
-  pass # No further functionality required
-  
+    pass  # No further functionality required
+
+
 class TableExistsException(Exception):
-  pass # No further functionality required
+    pass  # No further functionality required
