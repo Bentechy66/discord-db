a63e422dc7723932f65bf56eaca4ce37c1ea57f2
BenTechy66 <sendbenspam@yahoo.co.uk>
2019-06-06 20:04:20 +0100

    p e p  8

--- discorddb/__init__.py
+++ discorddb/__init__.py
@@ -1,8 +1,8 @@
-'''Main file for discord-db
+""""Main file for discord-db
 Please, never use this in a real project
 Ever
 Seriously
-Consider yourself warned'''
+Consider yourself warned"""
 
 from .classes import *
 
